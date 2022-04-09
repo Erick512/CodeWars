b3163d333093ad07fdfd6e03b57df5fe7b278a03
Erick512 <72674082+Erick512@users.noreply.github.com>
2022-04-08 23:56:33 -0700

    kata solved

---
+++ JavaScript/8 kyu/convertNumberToString.js
@@ -0,0 +1,15 @@
+/*
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+*/
+
+function numberToString(num) {
+    // Return a string of the number here!
+    return num.toString()
+  }
+
+// Sample Test
+
+console.log(numberToString(5))
+console.log(numberToString(1234))
