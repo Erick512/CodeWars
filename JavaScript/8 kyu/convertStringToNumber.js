4041c2e2d32e0c3189bd34234e929533f6e41c57
Erick512 <72674082+Erick512@users.noreply.github.com>
2022-04-20 18:29:11 -0700

    kata solved

---
+++ JavaScript/8 kyu/convertStringToNumber.js
@@ -0,0 +1,11 @@
+/*
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+*/
+
+var stringToNumber = function(str){
+    return +str
+  }
+
+  // Samle test
+  
+  console.log(stringToNumber('232'))
