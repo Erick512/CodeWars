6087bb45ac2ea7985cb1d1169b22c9d65d338d8b
Erick512 <72674082+Erick512@users.noreply.github.com>
2022-04-24 23:03:38 -0700

    kata solved

---
+++ JavaScript/8 kyu/youCantCodeUnderPressure.js
@@ -0,0 +1,11 @@
+/*
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+function doubleInteger(i){
+    return i + i
+}
+
+// Sample Tests
+
+console.log(doubleInteger(2))
