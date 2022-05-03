c002765c50b9c024edfba46235171f685bc90aa6
Erick512 <72674082+Erick512@users.noreply.github.com>
2022-05-03 15:23:41 -0700

    added sample test to kata

--- JavaScript/8 kyu/ifYouCantSleepCountSheep.js
+++ JavaScript/8 kyu/ifYouCantSleepCountSheep.js
@@ -1,3 +1,10 @@
+/*
+Task:
+
+Given a non-negative integer, 3 for example, 
+return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+*/
+
 var countSheep = function (num) {
     let str = ''
     for(let i = 1; i <= num; i++){
@@ -5,3 +12,7 @@ var countSheep = function (num) {
     }
     return str
 }
+
+// Sample tests
+
+console.log(countSheep(5))
