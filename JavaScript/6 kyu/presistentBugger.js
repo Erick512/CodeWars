c49c1309ac643e95b8cea187b3482d12df1425c7
Erick512 <72674082+Erick512@users.noreply.github.com>
2022-05-11 00:40:32 -0700

    kata solved

--- JavaScript/6 kyu/presistentBugger.js
+++ JavaScript/6 kyu/presistentBugger.js
@@ -1,16 +1,17 @@
-function presistence(num){
+/*
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit
+*/
 
+function persistence(num){
     if(num < 10) return 0
-
     let i = 0
-
     while(num.toString().length > 1){
         num = num.toString().split('').reduce((a, b) => a * b)
-        console.log(num)
         i++
     }
     return i
-    
 }
+
+// sample tests
 
 console.log(presistence(999))
