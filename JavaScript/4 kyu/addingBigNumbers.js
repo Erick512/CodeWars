5fad42c5d0b7a4f151fc9214e67ba77ca3a84ea6
Erick512 <72674082+Erick512@users.noreply.github.com>
2022-05-14 06:01:52 -0700

    kata solved

---
+++ JavaScript/4 kyu/addingBigNumbers.js
@@ -0,0 +1,38 @@
+function add(a, b){
+    let firstNum = [...a]
+    let secondNum = [...b]
+
+    let difference = Math.abs(firstNum.length - secondNum.length)
+    let extrZeros = [...Array(difference).keys()].map(x => '0')
+
+    if(firstNum.length > secondNum.length){
+       secondNum = extrZeros.concat(secondNum)
+    } else {
+        firstNum = extrZeros.concat(firstNum)
+    }
+
+    let arr = []
+    let carry = 0
+    
+    for(let i = firstNum.length - 1; i >= 0; i--){
+        for(let j = i; j >= 0; j--){
+
+            let res =  +firstNum[j] + +secondNum[j] + carry
+            res = res.toString().split('')
+
+            if(res.length > 1 && i != 0) {
+                arr.unshift(res[1])
+                carry = +res[0]
+            } else {
+                carry = 0
+                arr.unshift(res.join(''))
+            }
+            break
+        }
+    }
+    return arr.join('')
+}
+
+console.log(add('1123', '9459'))
+console.log(add('63829983432984289347293874', '90938498237058927340892374089'))
+// "91002328220491911630239667963"
