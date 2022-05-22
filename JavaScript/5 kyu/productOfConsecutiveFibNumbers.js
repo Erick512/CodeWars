cce881d1ed942cbd9e7aafaea04fe7ec04223cd0
Erick Alonso <alonsoerick@icloud.com>
2022-05-22 14:19:34 -0700

    kata solved

---
+++ JavaScript/5 kyu/productOfConsecutiveFibNumbers.js
@@ -0,0 +1,40 @@
+/*
+The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+such as
+
+F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+F(n) * F(n+1) = prod.
+
+Your function productFib takes an integer (prod) and returns an array:
+
+[F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+depending on the language if F(n) * F(n+1) = prod.
+
+If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+[F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+F(n) being the smallest one such as F(n) * F(n+1) > prod.
+*/
+
+function productFib(prod){
+    let numbers = [0, 1]
+
+    while(numbers[0] * numbers[1] <= prod){
+
+    if(numbers[0] * numbers[1] === prod) {return [numbers[0], numbers[1], true]}
+
+    numbers = [numbers[1], numbers[0] + numbers [1]]
+    }
+
+    return [numbers[0], numbers[1], false]
+}
+
+// Example
+
+console.log(productFib(714))
