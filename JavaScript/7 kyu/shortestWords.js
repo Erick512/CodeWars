934985d53ba6cf1f9ad744d80f4a541a731c99ce
Erick Alonso <alonsoerick@icloud.com>
2022-05-31 16:50:49 -0700

    kata solved

---
+++ JavaScript/7 kyu/shortestWords.js
@@ -0,0 +1,13 @@
+/*
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+function findShort(s){
+    return s.split(' ').map(x => x.length).sort((a, b) => a-b)[0]
+}
+
+// example
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
