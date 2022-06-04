dfdd21046701a1b0de6fcc3707061a016fcee91d
Erick Alonso <alonsoerick@icloud.com>
2022-06-04 13:04:13 -0700

    kata solved

---
+++ JavaScript/7 kyu/testing123.js
@@ -0,0 +1,15 @@
+/*
+Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+*/
+
+var number = function(array){
+    return array.map((x, i) => x = `${i + 1}: ${x}`)
+}
+
+// tests
+
+console.log(array['a', 'b', 'c'])
