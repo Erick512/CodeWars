8aca8b0521840805cf2e6bd54eb22e9cb0261907
Erick Alonso <alonsoerick@icloud.com>
2022-06-09 21:41:24 -0700

    kata solved

---
+++ JavaScript/8 kyu/abbreviateTwoWordNames.js
@@ -0,0 +1,12 @@
+/*
+Write a function to convert a name into initials. 
+This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+*/
+
+function abbrevName(name){
+    return name.split(' ').map(x => x[0]).join('.').toUpperCase()
+}
+
+console.log(abbrevName('eick alonos'))
