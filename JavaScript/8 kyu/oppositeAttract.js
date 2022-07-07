1fec796b520de1155640088535b422f7c1621ea2
Erick Alonso <alonsoerick@icloud.com>
2022-07-06 23:51:40 -0700

    kata solved

--- JavaScript/8 kyu/oppositeAttract.js
+++ JavaScript/8 kyu/oppositeAttract.js
@@ -2,6 +2,8 @@ function lovefunc(flower1, flower2){
     return (flower1 % 2 == 0 && flower2 % 2 !== 0) || (flower1 % 2 !== 0 && flower2 % 2 == 0)
 }
 
+// tests
+
 console.log(lovefunc(1,4))
 console.log(lovefunc(2,2))
 console.log(lovefunc(0,1))
