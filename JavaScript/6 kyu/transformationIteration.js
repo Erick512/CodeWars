6a3ce554f1dd1256a9cf4a6f7a59c9de3103a1c9
Erick Alonso <alonsoerick@icloud.com>
2022-08-05 16:06:51 -0700

    solved kata

--- JavaScript/6 kyu/transformationIteration.js
+++ JavaScript/6 kyu/transformationIteration.js
@@ -1,9 +1,33 @@
-// Write a function that multiplies two numbers
+// Parameters: Two Strings, same length
 
-function multiply(num1, num2) {
-    return num1 * num2
+// Returns: array of transformation from string 1 to string 2
+
+// Example: (car, see) => [sar, ser, see]
+
+/* Pseudo Code: 
+
+function that takes in two string
+iterate over first string replace each element , with the same index element form the second string 
+convert into array
+return array
+
+*/
+
+
+// Code
+
+function transform(source, traget) {
+    let arr = []
+    sourceCopy = source.split('')
+
+    for(let i = 0; i < source.length; i++) {
+        sourceCopy[i] = traget[i]
+        console.log(sourceCopy)
+    }
+
+    return arr
 }
 
-// tests
+// Tests
 
-console.log(multiply(2, 4))
+console.log(transform('car', 'see'))
