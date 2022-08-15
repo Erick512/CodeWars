656bb272bf9782373d2b08c367915daefe217c49
Erick Alonso <alonsoerick@icloud.com>
2022-08-14 17:05:05 -0700

    kata solved correctly

--- JavaScript/8 kyu/larioAndMuigi.js
+++ JavaScript/8 kyu/larioAndMuigi.js
@@ -1,6 +1,11 @@
 function pipeFix(numbers){
-    return [...Array(numbers[numbers.length - 1])].map((_, i) => i + 1)
+    let arr = []
+    for(let i = numbers[0]; i <= numbers[numbers.length - 1] ; i++) {
+        arr.push(i)
+    }
+    return arr
 }
 
 console.log(pipeFix([3,5,7,8]))
-console.log(pipeFix([5,7,12]))
+console.log(pipeFix([-5,7,12]))
+console.log(pipeFix([-1,4]))
