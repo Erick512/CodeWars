91a82e28f5755223c22b4700e4c589e006f264b5
Erick Alonso <alonsoerick@icloud.com>
2022-08-23 17:42:05 -0700

    kata solved

---
+++ JavaScript/7 kyu/getTheMiddleCharacter.js
@@ -0,0 +1,17 @@
+/*
+You are going to be given a word. Your job is to return the middle character of the word. 
+If the word's length is odd, return the middle character. 
+If the word's length is even, return the middle 2 characters
+*/
+
+function getMiddle(s) {
+    let len = s.length
+    let even = (len % 2 == 0) 
+    let arr = s.split('')
+
+    return even ? `${arr[len/2 - 1]}${arr[len/2]}` : arr[Math.ceil(len/2) - 1]
+}
+
+console.log(getMiddle('ted'))
+console.log(getMiddle('brave'))
+console.log(getMiddle('late'))
