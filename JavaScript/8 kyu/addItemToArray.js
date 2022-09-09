3fbd6b2ffbf6173767779be48bf108627f6c1baf
Erick Alonso <alonsoerick@icloud.com>
2022-09-09 11:29:22 -0700

    kata sovled

---
+++ JavaScript/8 kyu/addItemToArray.js
@@ -0,0 +1,14 @@
+/*
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+*/
+
+// tests
+
+var websites = []
+
+websites.push('codewars')
+
+console.log(websites)
