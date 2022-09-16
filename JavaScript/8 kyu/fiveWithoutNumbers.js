e2252623fdd8e57db6415e7b946b2acdf7e38af8
Erick Alonso <alonsoerick@icloud.com>
2022-09-16 14:30:46 -0700

    kata solved

---
+++ JavaScript/8 kyu/fiveWithoutNumbers.js
@@ -0,0 +1,15 @@
+/*
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+*/
+
+function unusualFive() {
+    return ['','','','',''].length
+}
+
+// tests
+
+console.log(unusualFive())
