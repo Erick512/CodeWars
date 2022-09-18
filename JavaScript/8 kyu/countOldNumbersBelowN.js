3edfd51de75119ba19d4e751bfd5a11c013a8adf
Erick Alonso <alonsoerick@icloud.com>
2022-09-18 14:40:13 -0700

    kata solved

---
+++ JavaScript/8 kyu/countOldNumbersBelowN.js
@@ -0,0 +1,13 @@
+/*
+Given a number n, return the number of positive odd numbers below n, EASY!
+*/
+
+function oddCount(n){
+
+    return Math.floor(n/2)
+
+}
+
+// tests
+
+console.log(oddCount(619817124721316))
