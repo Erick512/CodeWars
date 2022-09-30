3a9371b085cd4b36431065eb1a228d536e0e8bfe
Erick Alonso <alonsoerick@icloud.com>
2022-09-30 15:48:26 -0700

    kata solved

---
+++ JavaScript/8 kyu/fakeBinary.js
@@ -0,0 +1,11 @@
+/*
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+*/
+
+function fakeBin(x){
+    return x.split('').map(x => x >= 5 ? '1' : '0').join('')
+}
+
+console.log(fakeBin('123456789'))
