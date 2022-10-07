d38378a1e062e6fbaddd1255ee8a1162f7798ad5
Erick Alonso <alonsoerick@icloud.com>
2022-10-07 15:18:37 -0700

    kata solved

---
+++ JavaScript/8 kyu/noLoops.js
@@ -0,0 +1,13 @@
+/*
+You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+
+Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+Looking for more, loop-restrained fun? Check out the other kata in the series:
+*/
+
+function check(a, x) {
+    return a.includes(x)
+}
+
+console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45))
--- JavaScript/8 kyu/welcomeToTheCity.js
+++ JavaScript/8 kyu/welcomeToTheCity.js
@@ -1,7 +1,6 @@
 /*
 Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
 
-
 */
 
 function sayHello( name, city, state ) {
@@ -9,5 +8,7 @@ function sayHello( name, city, state ) {
     return `Hello ${name}! Welcome to ${city}, ${state}!`
 
 }
+
+//tests 
 
 console.log(sayHello('Erick', 'Los Angeles', 'California'))
