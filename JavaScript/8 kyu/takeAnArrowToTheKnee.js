24071d8a27199da7d4a09e18bee99aab3be5b7bb
Erick Alonso <alonsoerick@icloud.com>
2022-10-15 16:08:04 -0700

    kata solved

---
+++ JavaScript/8 kyu/takeAnArrowToTheKnee.js
@@ -0,0 +1,13 @@
+/*
+Come here to practice the Arrow style functions Not much else to say good luck!
+Details
+
+You will be given an array of numbers which can be used using the String.fromCharCode() (JS), Tools.FromCharCode() (C#) method to convert the number to a character. It is recommended to map over the array of numbers and convert each number to the corresponding ascii character.
+
+*/
+
+var ArrowFunc = function(arr) {
+    return arr.map( x => String.fromCharCode(x))
+}
+
+console.log(ArrowFunc([34, 342, 65]))
