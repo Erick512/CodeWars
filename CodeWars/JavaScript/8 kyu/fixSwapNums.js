3edcfe52904e7576b6918794d564ff6e6588fefb
Erick Alonso <alonsoerick@icloud.com>
2022-10-22 14:19:00 -0700

    kata solved

---
+++ CodeWars/JavaScript/8 kyu/fixSwapNums.js
@@ -0,0 +1,12 @@
+/*
+I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+Can you figure out what's wrong here?
+*/
+
+function swapValues() {
+    var args = Array.prototype.slice.call(arguments);
+    var temp = args[0][0];
+    args[0][0] = args[0][1];
+    args[0][1] = temp;
+}
