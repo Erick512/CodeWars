04ba8db529d8bf1d49ce7c83f08c3593d733bd40
Erick Alonso <alonsoerick@icloud.com>
2022-11-07 03:23:16 -0800

    kata solved

--- CodeWars/JavaScript/7 kyu/smallEnough.js
+++ CodeWars/JavaScript/7 kyu/smallEnough.js
@@ -1,5 +1,11 @@
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
 function smallEnough(a, limit) {
     return Math.max(...a) <= limit
 }
+
+// tests
 
 console.log(smallEnough([66, 101], 8))
