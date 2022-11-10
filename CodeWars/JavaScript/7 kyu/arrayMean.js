2fd3389c1c408470a88ca54705d327694334d3c7
Erick Alonso <alonsoerick@icloud.com>
2022-11-10 03:27:20 -0800

    kata sovled

--- CodeWars/JavaScript/7 kyu/arrayMean.js
+++ CodeWars/JavaScript/7 kyu/arrayMean.js
@@ -1,5 +1,10 @@
+// Find the mean (average) of a list of numbers in an array
+
+
 var findAverage = function (nums) {
     return nums.reduce((a, b) => a + b)/nums.length
 }
+
+// tetsts
 
 console.log(findAverage([1,3,5,7]))
