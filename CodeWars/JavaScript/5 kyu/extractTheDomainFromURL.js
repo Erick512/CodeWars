7280735f2cca1aa9ec6d4f9cb97368b1483bf81c
Erick Alonso <alonsoerick@icloud.com>
2022-11-12 03:51:13 -0800

    kata sovled

--- CodeWars/JavaScript/5 kyu/extractTheDomainFromURL.js
+++ CodeWars/JavaScript/5 kyu/extractTheDomainFromURL.js
@@ -1,3 +1,8 @@
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+// * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+// * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+// * url = "https://www.cnet.com"                -> domain name = cnet"
+
 function domainName(url){
     return url.replace('https://', '')
                 .replace('http://', '')
@@ -5,5 +10,9 @@ function domainName(url){
                 .split('.')[0]
 }
 
+// tests
+
 console.log(domainName('http://github.com/carbonfive/raygun'))
 console.log(domainName('http://www.github.com/carbonfive/raygun'))
+console.log(domainName('https://www.github.com/carbonfive/raygun'))
+console.log(domainName('www.github.com/carbonfive/raygun'))
